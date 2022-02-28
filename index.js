a5efb4369f0a3d434814af6048b286d8c33c9846
Hessam Imen <hessam.imen@gmail.com>
2022-02-28 19:54:25 +1100

    Done with assignment

---
+++ index.js
@@ -0,0 +1,27 @@
+function add(a, b){
+    return a + b;
+}
+function subtract(a, b){
+    return a - b;
+}
+function multiply(a, b){
+    return a * b;
+}
+function divide(a, b){
+    return a / b;
+}
+function increment(n){
+    n++;
+    return n;
+}
+function decrement(n){
+    n--;
+    return n;
+}
+function makeInt(n, base=10){
+    return parseInt(n,base)
+}
+function preserveDecimal(n){
+    return parseFloat(n)
+}
+
